a820140c4b073a5bd642ffac8855c7dbba95aa47
romanyukeduard <romanyuk.eduard@yahoo.com>
2016-03-03 19:10:22 +0200

    Add fab menu to place inform

--- app/src/main/java/io/github/romanyukeduard/tripguider/PlaceActivity.java
+++ app/src/main/java/io/github/romanyukeduard/tripguider/PlaceActivity.java
@@ -1,27 +1,28 @@
 package io.github.romanyukeduard.tripguider;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.squareup.picasso.Picasso;
 
 public class PlaceActivity extends AppCompatActivity{
 
     String url = "http://images4.fanpop.com/image/photos/21200000/Lutsk-castle-ukraine-21240842-620-465.jpg";
     Toolbar mToolbar;
-    FloatingActionButton mFAB;
-    boolean fav = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.place_layout);
+
+        initFabMenu();
 
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mToolbar.setTitle("Place name");
@@ -31,23 +32,32 @@ public class PlaceActivity extends AppCompatActivity{
         ImageView col = (ImageView) findViewById(R.id.backdrop);
         Picasso.with(this).load(url).into(col);
 
-        mFAB = (FloatingActionButton) findViewById(R.id.fab_fav);
-        mFAB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(fav == false){
-                    mFAB.setImageDrawable(getResources().getDrawable(R.drawable.ic_heart));
-                    fav = true;
-                }else{
-                    mFAB.setImageDrawable(getResources().getDrawable(R.drawable.ic_heart_outline));
-                    fav = false;
-                }
-            }
-        });
-
         testCard("Pizza", "Street", "10 km", "099 1488 228");
 
     }
+
+    private void initFabMenu() {
+
+        final FloatingActionButton fav = (FloatingActionButton) findViewById(R.id.fab_fav);
+        fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(PlaceActivity.this, "fav", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        final FloatingActionButton plan = (FloatingActionButton) findViewById(R.id.fab_plan);
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast1 = Toast.makeText(PlaceActivity.this, "plan", Toast.LENGTH_SHORT);
+                toast1.show();
+            }
+        });
+
+    }
+
 
     public void testCard(final String intro, final String address, final String distance, final String number){
         LinearLayout cIntro = (LinearLayout) findViewById(R.id.intro);
